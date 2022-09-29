319ae7f48655a951debf95d0f8cbe1c67ea654a1
Hiro <hiroyukitsune4@gmail.com>
2022-09-29 11:56:19 -1200

    added

---
+++ Coding Excercise 5/Question1.js
@@ -0,0 +1,12 @@
+// Question 1 Write a function to find the index of the value in the 2D Array
+
+
+let numList = [[1,2,3], [4,5,6], [7,8,9]];
+
+function findIndex(array, target) {
+    // Write the logic
+
+    return [0, 1] // Here 0 is the index of outer array, and 1 is the index of inner array
+}
+
+// Example - findINdex(numList, 5) // [1, 1]
---
+++ Coding Excercise 5/Question2.js
@@ -0,0 +1,12 @@
+// Question 2 Write a function that takes a string, and capitalizes every first letter of the word
+
+let str = "hello world";
+
+function capitlize(str) {
+    // return's first capitalized
+}
+
+capitlize(str); // Hello World
+
+// HINT - .split method on a string
+// .join method on the array
