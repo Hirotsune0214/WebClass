30c8201920c27fdc6affaef06d293d96dd9244e8
Hiro <hiroyukitsune4@gmail.com>
2022-10-01 22:24:16 -1200

    added

--- スラスラわかるJavascript/callback.js
+++ スラスラわかるJavascript/callback.js
@@ -1,31 +1,43 @@
-/*callback関数は後でして欲しい指示
-  コールバック関数とは、ある関数を呼び出す時に、
-  引数に指定する別の関数のことです
-  関数の中で関数を利用する
-
-高階関数 = A関数をB関数の引数に渡すことが出来る
-
-
-// A関数(コールバックとして実行される関数)
-function hello(){
-    // 実行されたら"hello"と出力する
-    console.log("hello");
+/*Callbackは今ではなく後でする指示
+  基本構文
+function 関数( 別の関数（callback） ) {
+ 
+    //何らかの処理を記述する
+ 
+    //引数に指定した関数を実行する
+    callback();
+ 
 }
 
-// B関数 (コールバックを実行する関数)
-function execute(callback){
-  // コールバックの実行
-  callback();
+function testFunc(callback) {
+ 
+  setTimeout(function() {
+    console.log('testFuncが実行されました');
+    callback();
+  }, 2000)
+ 
 }
 
-// hello関数を引数にして、execute関数を実行する
-execute(hello);
+function myCallback() {
+ 
+  console.log('myCallbackが実行されました');
 
-22行目で「execute」関数を実行。その際に引数として「hello」関数を渡す
-16行目の「execute」関数が呼ばれて、引数の「callback」変数には「hello」関数が入る
-18行目で「callback」変数の内容は「hello」関数であるため、「callback()」のように「()」（カッコ）をつけて関数を実行する
-10行目のhello関数が呼ばれる
-12行目のconsole.log関数で “hello” という文字列を出力する
+}
+
+testFunc(myCallback);
+
+
+let a = 3;  let b = 2; let c = a; 
+a=b=c=1
+
+
+console.log(a);*/
+
+// Callback Functions
+
+// When a function is passed as a parameter to another function its called
+// as funciton callback
+
 
 function printName() {
   console.log("prabh");
@@ -42,42 +54,31 @@ function greet(callback) {
 
 greet(printName);
 
-1.greet(printName);が呼ばれる
-2.greetがある35行目が呼ばれる。
-　greet(callback)→greet(printName)になる。
-3.38行目のcallbackの変数はprintNameで、関数が実行される。
-4.30行目のprintName()が呼ばれる。
-5.31行目のconsole.logが呼ばれる
+// call greet with printname as a paramater
+// call the parameter which is printName
+// console.log(Prabh)
 
 
-// 高階関数
-function main(fn){
-    fn("コールバックに渡す引数だよ"); 
-}
 
-//コールバック関数
-function sub(arg){
-    alert(arg);
-}
+ /*Multiple parameters as functions
+   高階関数 = Higher order function
+  
 
-//高階関数に関数を渡して実行
-main(sub);
-*/
-
-//高階関数
+高階関数
 function a(funB, funC) {
   funB();
   funC();
 }
 
-//コールバック関数
+普通の関数(コールバック関数)
 function b() {
   console.log("I am B");
 }
-//コールバック関数
+
+普通の関数(コールバック関数)
 function c() {
   console.log("I am C");
 }
 
-a(b, c);
-//高階関数の関数を記載aして()内にはcallbackに使用する関数(b, c)を記入
+高階関数にコールバック関数を渡して実行
+a(b, c);*/
