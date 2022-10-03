9d0f9040c25a4de5be2c041fc17662cff413cd5c
Hiro <hiroyukitsune4@gmail.com>
2022-10-02 22:36:58 -1200

    added

--- Coding_Excercise_5/Question1.js
+++ Coding_Excercise_5/Question1.js
@@ -1,32 +1,30 @@
 // Question 1 Write a function to find the index of the value in the 2D Array
-
-
-function findIndex(array, target) {
-
-    for(let i = 0; i < array.length; i++){
-        for(let j = 0; j < array[i].length-1; j++){
-            return [array[i][j]] = array[i][j + 1];  // Here 0 is the index of outer array, and 1 is the index of inner array
-        } //for
-    } //for
-    return  [0, 1];
-} //function
-
-// findINdex(numList, 5);
-let numList = [[1,2,3], [4,5,6], [7,8,9]];
-console.log(findIndex(array, 5));
-
 // Example - findINdex(numList, 5) // [1, 1]
 
-/*function find2d(matrix, item) {
-    let ix = 0, col = -1;
-    while (ix < matrix.length && (col = matrix[ix].indexOf(item)) === -1) ix++;
-    return ix === matrix.length ? undefined : [ix, col];
-}
-let matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
+function findIndex(array, target) {
+  //if array doesn't have a number then return [ -1 , -1]
+  let result = [ -1 , -1];
+    // Rows 行
+    for(let i = 0; i < numList.length; i++){
+        // column 列
+        for(let j = 0; j < numList[i].length; j++){
+            // if target is found, return result
+            if(array[i][j] == target){
+              // Why result[0][1]?? Because of 2D array?/
+              result[0] = i;
+              result[1] = j;
+              return result;
+            }  
+         // Here 0 is the index of outer array, and 1 is the index of inner array
+        } //for
+    } //for
+    // if target isn't found, return line 6's parameter
+    return result;
+} //function
 
 
-*/
+let numList = [[1,2,3], [4,5,6], [7,8,9]];
+console.log(findIndex(numList, 4));
+//findINdex(numList, 5);
+
+
--- Coding_Excercise_5/Question2.js
+++ Coding_Excercise_5/Question2.js
@@ -1,12 +1,31 @@
 // Question 2 Write a function that takes a string, and capitalizes every first letter of the word
 
-let str = "hello world";
+// let str = "hello world";
 
-function capitlize(str) {
-    // return's first capitalized
-}
+// function capitlize(str) {
+//     // return's first capitalized
+// }
 
-capitlize(str); // Hello World
+// capitlize(str); // Hello World
 
 // HINT - .split method on a string
 // .join method on the array
+
+{
+    const capitalizeName = function(name) {
+      //空白で分ける
+      const names = name.split(' ');
+      //下記をする意味??
+      const namesUpper = [];
+      //namesをnに渡す??
+      for(const n of names) {
+        // namesUpper.push(n[0].toUpperCase() + n.slice(1));
+        //上の方法か下の方法で最初の文字を大文字にできる
+        //n[0]は[H]と[W]をreplaceし、大文字に変換
+        namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
+      }
+      //連結した文字列を新たに作成して返す。
+      console.log(namesUpper.join(' '));
+    }
+    capitalizeName('hello world');
+    }
